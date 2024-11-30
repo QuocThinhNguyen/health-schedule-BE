47b6b24cd6e1729dc8d028bfb731df165df9d1b6
QuocThinhNguyen <nguyenquocthinh111@gmail.com>
2024-11-30 23:16:37 +0700

    chuc nang phan specialty

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import allCodeRouter from "./AllCodeRouter.js"
 import userRouter from "./UserRouter.js"
 import bookingRouter from "./BookingRouter.js"
 import clinicRouter from "./ClinicRouter.js"
+import specialtyRouter from "./SpecialtyRouter.js"
 import {handleError} from "../middlewares/authMiddleware.js"
 
 const routes = (app) => {
@@ -11,6 +12,8 @@ const routes = (app) => {
     app.use('/user', userRouter)
     app.use('/booking',bookingRouter)
     app.use('/clinic', clinicRouter)
+    app.use('/specialty', specialtyRouter)
+
     app.use(handleError);
 }
 

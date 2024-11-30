60b62c894f1b104375ea7852cb978562abe648de
T-Huy <lehuy099@gmail.com>
2024-11-30 22:55:00 +0700

    Update user

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,43 +1,43 @@
 import express from "express";
-import {
-  createUserController,
-  loginUserController,
-  updateUserController,
-  deleteUserController,
-  getAllUserController,
-  getDetailsUserController,
-  refreshToken,
-  logoutUserController,
-  resetUserPasswordController,
-  handleResetPasswordTokenController,
-  verifyUserController,
-  createAndSendOTPController,
-  getUserByNameOrEmailController,
-  updatePasswordController,
-  getDropdownUsersController
-} from "../controllers/UserController.js";
+import userController from "../controllers/UserController.js";
 import {
   authAdminMiddleware,
   authMiddleware,
-  authUserMiddleware,
 } from "../middlewares/authMiddleware.js";
 import upload from "../utils/fileUpload.js";
 
 const router = express.Router();
 
 //CRUD user
-router.get("/dropdown", authAdminMiddleware, getDropdownUsersController); //Lấy dropdown user
-router.get("/search", authAdminMiddleware, getUserByNameOrEmailController); //Tìm kiếm user theo tên, email
-router.get("/", authAdminMiddleware, getAllUserController); //Lấy tất cả user
-router.get("/:id", authMiddleware, getDetailsUserController); //Lấy thông tin một user
-router.post("/", authAdminMiddleware, upload.single("image"), createUserController); //Thêm user
+router.get(
+  "/dropdown",
+  authAdminMiddleware,
+  userController.getDropdownUsersController
+); //Lấy dropdown user
+router.get(
+  "/search",
+  authAdminMiddleware,
+  userController.getUserByNameOrEmailController
+); //Tìm kiếm user theo tên, email
+router.get("/", authAdminMiddleware, userController.getAllUserController); //Lấy tất cả user
+router.get("/:id", authMiddleware, userController.getDetailsUserController); //Lấy thông tin một user
+router.post(
+  "/",
+  authAdminMiddleware,
+  upload.single("image"),
+  userController.createUserController
+); //Thêm user
 router.put(
   "/:id",
   authMiddleware,
   upload.single("image"),
-  updateUserController
+  userController.updateUserController
 ); //Cập nhật user
-router.delete("/:id", authAdminMiddleware, deleteUserController); //Xóa user
-router.post("/update-password", authMiddleware, updatePasswordController); // Định nghĩa tuyến đường để cập nhật mật khẩu
+router.delete("/:id", authAdminMiddleware, userController.deleteUserController); //Xóa user
+router.post(
+  "/update-password",
+  authMiddleware,
+  userController.updatePasswordController
+); // Định nghĩa tuyến đường để cập nhật mật khẩu
 
 export default router;
--- src/routes/WebRouter.js
+++ src/routes/WebRouter.js
@@ -1,33 +1,17 @@
 import express from "express";
-import {
-    createUserController,
-    loginUserController,
-    updateUserController,
-    deleteUserController,
-    getAllUserController,
-    getDetailsUserController,
-    refreshToken,
-    logoutUserController,
-    resetUserPasswordController,
-    handleResetPasswordTokenController,
-    verifyUserController,
-    createAndSendOTPController,
-    getUserByNameOrEmailController,
-} from "../controllers/UserController.js";
-import {
-    authAdminMiddleware,
-    authUserMiddleware,
-} from "../middlewares/authMiddleware.js";
-import upload from "../utils/fileUpload.js";
+import userController from "../controllers/UserController.js";
 
 const router = express.Router();
 
-router.post("/sign-up", createAndSendOTPController);
-router.post("/verify-account/:token", verifyUserController);
-router.post("/sign-in", loginUserController);
-router.post("/logout", logoutUserController);
-router.post("/reset-password", resetUserPasswordController);
-router.get("/reset-password/:token", handleResetPasswordTokenController);
-router.post("/refresh_token", refreshToken);
+router.post("/sign-up", userController.createAndSendOTPController);
+router.post("/verify-account/:token", userController.verifyUserController);
+router.post("/sign-in", userController.loginUserController);
+router.post("/logout", userController.logoutUserController);
+router.post("/reset-password", userController.resetUserPasswordController);
+router.get(
+  "/reset-password/:token",
+  userController.handleResetPasswordTokenController
+);
+router.post("/refresh_token", userController.refreshToken);
 
 export default router;

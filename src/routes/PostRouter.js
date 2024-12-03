d184a795c64ed1a3d977350ba6b33883a22b954a
T-Huy <lehuy099@gmail.com>
2024-12-03 17:20:45 +0700

    CRUD post

---
+++ src/routes/PostRouter.js
@@ -0,0 +1,14 @@
+import express from "express";
+import postController from "../controllers/PostController.js";
+import upload from "../utils/fileUpload.js";
+import { authAdminMiddleware } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/", postController.getAllPost);
+router.get("/:id", postController.getPostById);
+router.post("/", upload.single("image"), postController.createPost);
+router.put("/:id", upload.single("image"), postController.updatePost);
+router.delete("/:id", postController.deletePost);
+
+export default router;

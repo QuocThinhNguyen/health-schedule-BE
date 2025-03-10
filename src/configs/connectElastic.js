cc8d55a5eff174741588b4b46e5745121d6e8815
T-Huy <lehuy099@gmail.com>
2025-03-10 22:08:56 +0700

    update min_gram, max_gram

--- src/configs/connectElastic.js
+++ src/configs/connectElastic.js
@@ -4,7 +4,7 @@ const elasticClient = new Client({
   node: "https://127.0.0.1:9200",
   auth: {
     username: 'elastic',
-    password: 'pOsx24ib9LEO5HyCYl1D'
+    password: 'La4DM3RlgrQv7nfJ*dtt'
     },
   tls: {
     rejectUnauthorized: false,

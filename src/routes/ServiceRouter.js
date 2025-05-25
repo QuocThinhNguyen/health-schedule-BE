d2d8aed3172dcdc03a09f21f1a83a0c73ea28190
T-Huy <lehuy099@gmail.com>
2025-05-26 03:33:07 +0700

    thêm lịch dịch vụ và chỉnh sửa quản lý lịch hẹn của bác sĩ ở role clinic

--- src/routes/ServiceRouter.js
+++ src/routes/ServiceRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import serviceController from "../controllers/ServiceController.js";
 import upload from "../utils/fileUpload.js";
-import { authAdminMiddleware } from "../middlewares/authMiddleware.js";
+import { authClinicMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", serviceController.getServiceBySearchAndFilter);
+router.get("/clinic",authClinicMiddleware, serviceController.getServiceByClinic);
 router.get("/:id", serviceController.getServiceById);
 router.post("/", upload.single("image"), serviceController.createService);
 router.put("/:id", upload.single("image"), serviceController.updateService);
